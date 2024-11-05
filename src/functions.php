edfe868729ccacfbb304910f9382d0fac1dd6c19
ipranjal <pranjaltech10@gmail.com>
2024-11-05 06:50:47 +0530

    feat: functions.php

---
+++ src/functions.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * This file is part of the Scrawler package.
+ *
+ * (c) Pranjal Pandey <its.pranjalpandey@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+ /*
+ * Define your custom helper functions here
+ */
